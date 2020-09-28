626b689abddbdafa33971fc3315292c84ddbfce1
Caio <caioap25@gmail.com>
2020-09-28 18:29:19 -0300

    First commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="nav" class="nav">
+    <router-link :to="{ name: 'Documentos', params: { userhash: 'qwerty' } }">
+      Documentos |
+    </router-link>
+
+    <nav>
+      <router-link :to="{ name: 'NewUser' }">
+        | Cadastro de Usuários
+      </router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+div#nav {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/views/UserDocuments.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Documentos do usuário #{{ userhash }}</h1>
+</template>
+
+<script>
+export default {
+  props: ['userhash']
+}
+</script>
+
+<style lang="scss" scoped></style>
